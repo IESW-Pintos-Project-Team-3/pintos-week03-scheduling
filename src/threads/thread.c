59b885beb8a108f384ad5ad1a531e151d8cc92ae
thisjheok <jhjang3344@gmail.com>
2025-07-14 13:39:09 +0900

    fix:next_thread_to_run 리턴 수정

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -558,7 +558,8 @@ next_thread_to_run (void)
   if (list_empty (&ready_list))
     return idle_thread;
   else
-    return list_entry (list_pop_front (&ready_list), struct thread, elem);
+    return lottery_scheduling();
+    /*list_entry (list_pop_front (&ready_list), struct thread, elem);*/
 }
 
 /* Completes a thread switch by activating the new thread's page
