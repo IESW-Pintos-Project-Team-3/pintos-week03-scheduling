312fd8e4b3e9a9e77b83dd7d900d8970030ef8b0
hkmaeng <hkmaeng9075@naver.com>
2025-07-14 14:53:24 +0900

    fix: scheduledtick remove

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -91,7 +91,6 @@ struct thread
     struct list_elem allelem;           /* List element for all threads list. */
 
     int64_t tick_to_awake;           /* Tick to awake. */
-    int64_t scheduled_ticks;              /* Tick when scheduled */
     int64_t pass;              /* Pass value how long thread get CPU */
 
     /* Shared between thread.c and synch.c. */
