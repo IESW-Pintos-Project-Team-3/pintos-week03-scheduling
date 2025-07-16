bbe42a6a577925f196e7c351bcdc23b020a842bb
hkmaeng <hkmaeng9075@naver.com>
2025-07-16 11:56:15 +0900

    "feat: change testcode"

--- src/tests/threads/scheduling-lottery.c
+++ src/tests/threads/scheduling-lottery.c
@@ -1,4 +1,4 @@
-/* Tests lottery scheduling with 3 threads having tickets 50, 100, 150.
+/* Tests stride scheduling with 3 threads having tickets 50, 100, 150.
    Shows how many times each thread gets CPU time. */
 
 #include <stdio.h>
@@ -10,104 +10,125 @@
 #include "threads/vaddr.h"
 #include "devices/timer.h"
 
-static thread_func lottery_thread;
 static struct lock count_lock;
-static int thread_counts[5] = {0, 0, 0, 0, 0}; // 각 스레드의 실행 횟수
-static int thread_tickets[5] = {50, 100, 150, 200, 250}; // 각 스레드의 티켓 수
+static int thread_counts[7] = {0, 0, 0, 0, 0, 0, 0}; // 각 스레드의 실행 횟수
+static int thread_tickets[7] = {400, 450, 500, 550, 600, 650, 700}; // 각 스레드의 티켓 수
 static bool test_finished = false;
+static int64_t start_tick;
+static int cnt = 0;
+static int tick_thread_counts[10][7] = {{0, 0, 0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0}};
+static inline uint64_t rdtsc(void) {
+  unsigned hi, lo;
+  __asm__ volatile ("rdtsc" : "=a"(lo), "=d"(hi));
+  return ((uint64_t)hi << 32) | lo;
+}
 
-struct lottery_data {
+struct thread_data {
   int id;
   int tickets;
 };
+
+static void lottery_schedule(void*);
 
 void
 test_lottery_scheduling (void) 
 {
   int i;
-  struct lottery_data data[5];
+  struct thread_data data[7];
   
   /* This test does not work with the MLFQS. */
   ASSERT (!thread_mlfqs);
 
   lock_init (&count_lock);
   
-  msg ("Starting lottery scheduling test with 3 threads:");
+  msg ("Starting lottery scheduling test with 5 threads:");
   msg ("Thread 0: 50 tickets");
-  msg ("Thread 1: 100 tickets"); 
-  msg ("Thread 2: 150 tickets");
+  msg ("Thread 1: 250 tickets");
+  msg ("Thread 2: 500 tickets"); 
+  msg ("Thread 3: 750 tickets");
+  msg ("Thread 4: 1000 tickets");
   
   /* Create 3 threads with different ticket counts */
-  for (i = 0; i < 5; i++) 
+  for (i = 0; i < 7; i++) 
     {
       char name[32];
-      snprintf (name, sizeof name, "lottery-thread-%d", i);
+      snprintf (name, sizeof name, "stride-thread-%d", i);
       
       data[i].id = i;
       data[i].tickets = thread_tickets[i];
       
-      thread_create (name, thread_tickets[i], lottery_thread, &data[i]);
+      thread_create (name, thread_tickets[i], lottery_schedule, &data[i]);
       msg ("Created thread %d with %d tickets", i, thread_tickets[i]);
     }
 
   /* Let threads run for a longer time */
-  msg ("Running lottery scheduling test for 6000 ticks...");
-  msg("start ticks = %lld", timer_ticks());
+  msg ("Running stride scheduling test for 6000 ticks...");
+  start_tick = timer_ticks();
+  uint64_t start_cycles = rdtsc();
   timer_sleep (6000);
 
   /* Signal threads to finish */
   test_finished = true;
+  uint64_t end_cycles = rdtsc();
+  msg("Total CPU cycles spent during scheduling: %llu\n", end_cycles - start_cycles);
   
   /* Wait a bit more for threads to finish */
   timer_sleep (100);
 
   /* Print results */
   lock_acquire (&count_lock);
-  msg ("Lottery scheduling results:");
-  
-  int total_runs = 0;
-  for (i = 0; i < 5; i++) {
-    total_runs += thread_counts[i];
-    msg ("Thread %d (tickets=%d): ran %d times", i, thread_tickets[i], thread_counts[i]);
-  }
-  
-  msg ("Total runs: %d", total_runs);
-  
+  msg ("lottery scheduling results:");
+    
   /* Show expected vs actual distribution (using integer arithmetic) */
   msg ("Expected vs Actual distribution:");
-  for (i = 0; i < 5; i++) {
-    int expected_percent = (thread_tickets[i] * 100) / 750;
-    int actual_percent = (thread_counts[i] * 100) / total_runs;
-    msg ("Thread %d: expected %d%%, actual %d%%", i, expected_percent, actual_percent);
+  for (i = 0; i < cnt; i++) {
+    int total_runs = 0;
+    for (int j = 0; j < 7; j++) {
+      total_runs += tick_thread_counts[i][j];
+      // msg ("Thread %d (tickets=%d): ran %d times", j, thread_tickets[j], tick_thread_counts[i][j]);
+    }
+    for(int j = 0;j< 7;j++){
+      int expected_percent = (thread_tickets[j] * 100) / 3850; // 250+500+750=1500
+      int actual_percent = (tick_thread_counts[i][j] * 100) / total_runs;
+      msg ("Thread %d: expected %d%%, actual %d%%", j, expected_percent, actual_percent);
+    }
   }
   
-  /* Check if lottery scheduling is working */
-  if (thread_counts[2] > thread_counts[1] && thread_counts[1] > thread_counts[0]) {
-    msg ("Lottery scheduling appears to be working correctly!");
-  } else {
-    msg ("Lottery scheduling may not be working as expected.");
-  }
+  /* Check if stride scheduling is working */
+  // if (thread_counts[4] > thread_counts[3] &&thread_counts[3] > thread_counts[2] &&thread_counts[2] > thread_counts[1] && thread_counts[1] > thread_counts[0]) {
+  //   msg ("Stride scheduling appears to be working correctly!");
+  // } else {
+  //   msg ("Stride scheduling may not be working as expected.");
+  // }
   
   lock_release (&count_lock);
 }
 
 static void
-lottery_thread (void *aux) 
+lottery_schedule (void *aux) 
 {
-  struct lottery_data *data = (struct lottery_data *)aux;
+  struct thread_data *data = (struct thread_data *)aux;
   int my_id = data->id;
   
   while (!test_finished) 
-    {
+    {  
       uint32_t *esp;
       asm ("mov %%esp, %0" : "=g" (esp));
       struct thread* t = pg_round_down (esp);
-      msg("thread %d is scheduled, scheduledtime = %lld",my_id,timer_ticks());
-      unsigned long old_random_num = t->random_num;
+      // printf("thread%d is scheduled, thread's pass = %lld\n", my_id, t->pass);
+      unsigned long old_random = t->random_num;
       lock_acquire (&count_lock);
       thread_counts[my_id]++;
+      int64_t timing = timer_ticks()-start_tick;
+      if (timing != 0 && timing%600 == 0 ){
+        for (int i = 0; i < 7; i++) {
+          tick_thread_counts[cnt][i] += thread_counts[i];
+          tick_thread_counts[cnt+1][i] = tick_thread_counts[cnt][i];
+        }
+        cnt++;
+      }
       lock_release (&count_lock);
-      while(old_random_num == t->random_num){
+      while(old_random == t->random_num){
         asm ("mov %%esp, %0" : "=g" (esp));
         t = pg_round_down (esp);
       }
